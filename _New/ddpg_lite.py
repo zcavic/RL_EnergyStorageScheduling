Fix electrical price calculation

_New/ddpg_lite.py
@@ -108,7 +108,8 @@ class DDPGAgentLite:
         self.actor.load_state_dict(torch.load("model_actor"))
         self.actor.eval()
 
-        state = self.environment.reset()
+        state = self.environment.reset(df_test)
+        electricity_price = self.environment.model_data_provider.get_electricity_price_for_day(self.environment.current_datetime)
         total_episode_reward = 0
         proposed_storage_powers = []
         actual_storage_powers = []
@@ -130,8 +131,7 @@ class DDPGAgentLite:
             state = next_state
 
         print('total_episode_reward', total_episode_reward)
-        plot_daily_results(24, proposed_storage_powers, actual_storage_powers, storage_socs,
-                           self.environment.model_data_provider.get_electricity_price())
+        plot_daily_results(24, proposed_storage_powers, actual_storage_powers, storage_socs, electricity_price)
 
     def _update(self):
         states, actions, rewards, next_states, dones = self.replay_buffer.sample(self.batch_size)

_New/model_data_provider.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import numpy as np
 from _New.battery_capacity_fade import calculate
 from _New.energy_storage_lite import EnergyStorageLite
@@ -11,8 +13,9 @@ class ModelDataProvider:
     def get_electricity_price_for(self, timestamp):
         return self._dataset.loc[timestamp, 'price_day_ahead']
 
-    def get_electricity_price(self):
-        return self._dataset['ElectricityPrice'].values
+    def get_electricity_price_for_day(self, timestamp):
+        end_day = timestamp + timedelta(hours=23)
+        return self._dataset.loc[timestamp:end_day, 'price_day_ahead'].values
 
     def create_energy_storage(self, datetime):
         capacity_fade = calculate(np.array(self._dataset.loc[:datetime, 'SOC']))