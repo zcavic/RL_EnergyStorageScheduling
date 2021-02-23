Fix capacity fade calculation

model/battery_capacity_fade.py
@@ -1,17 +1,17 @@
 import math
 import rainflow
-from pandas import np
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def calculate(soc_series):
-    if len(soc_series) == 0:
+    if soc_series is None or len(soc_series) <= 1:
         return 0
     f_c = 0
-    for rng, mean, count, i_start, i_end in rainflow.extract_cycles(soc_series):
+    for rng, mean, count, i_start, i_end in rainflow.extract_cycles(soc_series):  # TODO da li je range isto sto i amplituda?
         soc_stress = _soc_stress(mean)
         dod_stress = _dod_stress(rng)
-        time_stress = _time_stress((i_end - i_start) * 60 * 60)
-        f_c = f_c + (count * soc_stress * dod_stress * time_stress)
+        f_c = f_c + (count * soc_stress * dod_stress)
 
     f_t = _time_stress(len(soc_series) * 3600) * _soc_stress(np.mean(soc_series))
 
@@ -29,7 +29,7 @@ def _dod_stress(rng):
     k_delta_1 = 1.40 * 10 ** 5
     k_delta_2 = -5.01 * 10 ** -1
     k_delta_3 = -1.23 * 10 ** 5
-    return (k_delta_1 * (2 * rng) ** k_delta_2 + k_delta_3) ** -1
+    return (k_delta_1 * rng ** k_delta_2 + k_delta_3) ** -1  # TODO proveriti da li treba 2, lici da ne treba
 
 
 def _time_stress(sec):
@@ -41,3 +41,20 @@ def _capacity_fade(f_c, f_t):
     alfa = 5.75 * 10 ** -2
     beta = 121
     return 1 - alfa * math.exp(-beta * (f_c + f_t)) - (1 - alfa) * math.exp(-(f_c + f_t))
+
+
+def test_calculation(soc_series):
+    test = calculate(soc_series)
+    capacity_fade = []
+    for i in range(len(soc_series)):
+        if i == 0:
+            capacity_fade.append(0)
+        elif i % 24 == 0:
+            capacity_fade.append(calculate(soc_series[:i]))
+
+    x_axis = [1 + j for j in range(len(capacity_fade))]
+    plt.plot(x_axis, capacity_fade)
+    plt.xlabel('time [days]')
+    plt.ylabel('capacity fade [%}')
+    plt.savefig("capacity_fade.png")
+    plt.show()

model/model_data_provider.py
@@ -18,6 +18,6 @@ class ModelDataProvider:
 
     def create_energy_storage(self, datetime):
         end_day = datetime-timedelta(hours=1)
-        capacity_fade = calculate(np.array(self._dataset.loc[:end_day, 'SOC']))
+        capacity_fade = calculate(np.array(self._dataset.loc[:end_day, 'SOC']))  # TODO dodati u dataset capacity fade
         soc = self._dataset.loc[datetime, 'SOC']
         return EnergyStorage(max_p_mw=1, max_e_mwh=6, initial_soc=soc, capacity_fade=capacity_fade)